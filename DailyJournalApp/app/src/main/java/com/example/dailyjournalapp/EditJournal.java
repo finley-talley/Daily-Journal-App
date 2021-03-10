6cc3daf30dd2142dd2c3b22acacce3fd1062d7ab
finley-talley <73396510+finley-talley@users.noreply.github.com>
2021-03-10 18:49:54 -0500

    Added the basic activities necessary for the project

---
+++ DailyJournalApp/app/src/main/java/com/example/dailyjournalapp/EditJournal.java
@@ -0,0 +1,14 @@
+package com.example.dailyjournalapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditJournal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_journal);
+    }
+}
