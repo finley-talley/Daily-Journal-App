6d9fd73fbb35181b4814c1b3d758d0f8abb68371
finley-talley <73396510+finley-talley@users.noreply.github.com>
2021-03-10 19:13:16 -0500

    Set up activity with TimePicker to choose notification time

--- DailyJournalApp/app/src/main/java/com/example/dailyjournalapp/NotificationActivity.java
+++ DailyJournalApp/app/src/main/java/com/example/dailyjournalapp/NotificationActivity.java
@@ -3,6 +3,7 @@ package com.example.dailyjournalapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TimePicker;
 
 public class NotificationActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class NotificationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification2);
+
+        TimePicker picker=(TimePicker)findViewById(R.id.timePicker1);
+        picker.setIs24HourView(false);
     }
 }
