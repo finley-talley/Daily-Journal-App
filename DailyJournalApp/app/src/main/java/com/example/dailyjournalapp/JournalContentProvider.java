f675ca71607aba4039d43cc4771d202c2b2e7021
finley-talley <73396510+finley-talley@users.noreply.github.com>
2021-04-10 20:02:35 -0400

    JournalContentProvider.java filled out

--- DailyJournalApp/app/src/main/java/com/example/dailyjournalapp/JournalContentProvider.java
+++ DailyJournalApp/app/src/main/java/com/example/dailyjournalapp/JournalContentProvider.java
@@ -2,49 +2,95 @@ package com.example.dailyjournalapp;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
 
 public class JournalContentProvider extends ContentProvider {
+    public final static String DBNAME = "JournalDB";
+    public final static String TABLE_NAME = "journaltable";
+    public final static String COLUMN_TITLE = "title";
+    public final static String COLUMN_TEXT = "text";
+    public final static String COLUMN_MOOD = "mood";
+    // leaving date/time out for now
+
+    public static final String AUTHORITY = "com.example.dailyjournalapp.provider";
+    public static final Uri CONTENT_URI = Uri.parse(
+            "content://com.example.dailyjournalapp.provider/" + TABLE_NAME);
+
+    private static UriMatcher sUriMatcher;
+
+    private MainDatabaseHelper mOpenHelper;
+
+    private static final String SQL_CREATE_MAIN = "CREATE TABLE " +
+            TABLE_NAME +  // Table's name
+            "(" +               // The columns in the table
+            " _ID INTEGER PRIMARY KEY, " +
+            COLUMN_TITLE + " TEXT, " +
+            COLUMN_TEXT + " TEXT, " +
+            COLUMN_MOOD + " INTEGER)";
+
+    protected static final class MainDatabaseHelper extends SQLiteOpenHelper {
+        MainDatabaseHelper(Context context) {
+            super(context, DBNAME, null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(SQL_CREATE_MAIN);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+        }
+    }
+
     public JournalContentProvider() {
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        // Implement this to handle requests to delete one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mOpenHelper.getWritableDatabase().delete(TABLE_NAME, selection, selectionArgs);
     }
 
     @Override
     public String getType(Uri uri) {
-        // TODO: Implement this to handle requests for the MIME type of the data
-        // at the given URI.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        // TODO: Implement this to handle requests to insert a new row.
-        throw new UnsupportedOperationException("Not yet implemented");
+        String title = values.getAsString(COLUMN_TITLE).trim();
+        String text = values.getAsString(COLUMN_TEXT).trim();
+
+        if (title.isEmpty() || text.isEmpty()) {
+            return null;
+        }
+
+        long id = mOpenHelper.getWritableDatabase().insert(TABLE_NAME, null, values);
+
+        return Uri.withAppendedPath(CONTENT_URI, "" + id);
     }
 
     @Override
     public boolean onCreate() {
-        // TODO: Implement this to initialize your content provider on startup.
-        return false;
+        mOpenHelper = new MainDatabaseHelper(getContext());
+        return true;
     }
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
-        // TODO: Implement this to handle query requests from clients.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mOpenHelper.getReadableDatabase().query(TABLE_NAME, projection, selection, selectionArgs,
+                null, null, sortOrder);
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection,
                       String[] selectionArgs) {
-        // TODO: Implement this to handle requests to update one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mOpenHelper.getWritableDatabase().update(TABLE_NAME, values, selection, selectionArgs);
     }
 }
